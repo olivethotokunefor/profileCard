d33c4b6131d90afe551392498eefedb80de1a819
olivethotokunefor <olivethotokunefor@gmail.com>
2025-10-18 21:57:39 -0700

    time update

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function updateTime() {
-  const currentTimeEl = document.getElementById("current-time");
-  const now = new Date();
-
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-
-  currentTimeEl.textContent = `${hours}:${minutes}:${seconds}`;
+  const timeElement = document.getElementById("current-time");
+  timeElement.textContent = Date.now();
 }
 
-setInterval(updateTime, 1000);
-updateTime();
+setInterval(updateTime, 1);
+
+
 document.querySelectorAll('.social-links a').forEach(link => {
   link.addEventListener('keydown', e => {
     if (e.key === 'Enter') {
